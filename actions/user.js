c3d326e3ec7925125b6e1dee7a0d02391c9a677b
Anurag Baranwal <anuragbaranwal789@gmail.com>
2025-03-12 00:16:21 +0530

    AI Integration & Dashboard

--- actions/user.js
+++ actions/user.js
@@ -2,8 +2,8 @@
 
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
-// import { revalidatePath } from "next/cache";
-// import { generateAIInsights } from "./dashboard";
+import { revalidatePath } from "next/cache";
+import { generateAIInsights } from "./dashboard";
 
 export async function updateUser(data) {
   const { userId } = await auth();
@@ -28,22 +28,13 @@ export async function updateUser(data) {
 
         // If industry doesn't exist, create it with default values
         if (!industryInsight) {
-        //   const insights = await generateAIInsights(data.industry);
+          const insights = await generateAIInsights(data.industry);
 
-          industryInsight = await tx.industryInsight.create({
+          industryInsight = await db.industryInsight.create({
             data: {
-            //   industry: data.industry,
-            //   ...insights,
-            //   nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-            industry: data.industry,
-            salaryRanges: [],
-            growthRate: 0,
-            demandLevel: "MEDIUM",
-            topSkills: [],
-            marketOutlook: "NEUTRAL",
-            keyTrends: [],
-            recommendedSkills: [],
-            nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+              industry: data.industry,
+              ...insights,
+              nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
             },
           });
         }
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,15 +1,22 @@
-import { redirect } from "next/navigation";
+import { getIndustryInsights } from "@/actions/dashboard";
 import { getUserOnboardingStatus } from "@/actions/user";
+import { redirect } from "next/navigation";
+import DashboardView from "./_components/dashboard-view";
 
-const IndustryInsightPage = async () => {
-    const { isOnboarded } = await getUserOnboardingStatus();
-    
-      if (!isOnboarded) {
-        redirect("/onboarding");
-      }
+export default async function DashboardPage() {
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  // If not onboarded, redirect to onboarding page
+  // Skip this check if already on the onboarding page
+  if (!isOnboarded) {
+    redirect("/onboarding");
+  }
+
+  const insights = await getIndustryInsights();
+
   return (
-    <div>IndustryInsightPage</div>
-  )
+    <div className="container mx-auto">
+      <DashboardView insights={insights} />
+    </div>
+  );
 }
-
-export default IndustryInsightPage
