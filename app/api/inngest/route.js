7baf90aed4b6386bfd352226523952c8c27d43a6
Anurag Baranwal <anuragbaranwal789@gmail.com>
2025-03-13 00:39:08 +0530

    Inngest

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,11 @@
 import { serve } from "inngest/next";
 
 import { inngest } from "@/lib/inngest/client";
-// import { generateIndustryInsights } from "@/lib/inngest/function";
-import { helloWorld } from "@/lib/inngest/functions";
+import { generateIndustryInsights } from "@/lib/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    // generateIndustryInsights
-    helloWorld
+    generateIndustryInsights
 ],
 });
